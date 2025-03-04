14d99084ead34dd75f44bfa2f7612a416e733df3
Wendy Balderrama <balderramawendym@gmail.com>
2025-03-04 18:09:17 -0400

    Feat: Agregue los saludos en idioma ingles + pruebas test

--- src/saludador.js
+++ src/saludador.js
@@ -1,37 +1,78 @@
-function saludador() {
-    return "Hola";
+function saludador(idioma) {
+    if(idioma==="español"){
+        return "Hola";
+    }else if(idioma==="ingles"){
+        return "Hello";
+    }
 }
 
-function saludadorConNombre(nombre) {
-    return `Hola ${nombre}`;
+function saludadorConNombre(nombre, idioma) {
+    if(idioma==="español"){
+        return `Hola ${nombre}`;
+    }else if(idioma==="ingles"){
+        return `Hello ${nombre}`;
+    }
 }
 
-function saludadorConHora(hora=new Date().getHours()){
-    if(hora<12){
-        return "Buenos dias";
-    }else if(hora<18){
-        return "Buenas tardes";
+function saludadorConHora(hora=new Date().getHours(), idioma){
+    if(idioma==="español"){
+        if(hora<12){
+            return "Buenos dias";
+        }else if(hora<18){
+            return "Buenas tardes";
+        }else{
+            return "Buenas noches";
+        }
     }else{
-        return "Buenas noches";
-    }
-}
-
-function saludadorSegunElGenero(nombre, genero){
-    if(genero==="Femenino"){
-        return `Hola, Sra. ${nombre}`;
-    }else if(genero==="Masculino"){
-        return `Hola, Sr. ${nombre}`;
-    }
-}
-
-function saludadorSegunEdadYGenero(nombre, edad, genero) {
-    if (edad > 30) {
-        if (genero === "Femenino"){
-            return `Hola, Sra. ${nombre}`;
-        } else if (genero === "Masculino") {
-            return `Hola, Sr. ${nombre}`;
+        if(hora<12){
+            return "Good morning";
+        }else if(hora<18){
+            return "Good afternoon";
+        }else{
+            return "Good evening";
         }
     }
-    return `Hola, ${nombre}`;
 }
+
+function saludadorSegunElGenero(nombre, genero, idioma){
+    if(idioma==="español"){
+        if(genero==="Femenino"){
+            return `Hola, Sra. ${nombre}`;
+        }else if(genero==="Masculino"){
+            return `Hola, Sr. ${nombre}`;
+        }
+    }else{
+        if(genero==="Female"){
+            return `Hello, Ms. ${nombre}`;
+        }else if(genero==="Male"){
+            return `Hello, Mr. ${nombre}`;
+        }
+    }
+}
+
+function saludadorSegunEdadYGenero(nombre, edad, genero,idioma) {
+
+    if(idioma==="español"){
+        if(edad>30){
+            if(genero==="Femenino"){
+                return `Hola, Sra. ${nombre}`;
+            }else if(genero==="Masculino"){
+                return `Hola, Sr. ${nombre}`;
+            }
+        }
+        return `Hola, ${nombre}`;
+    }else if(idioma==="ingles"){
+        if(edad>30){
+            if(genero==="Female"){
+                return `Hello, Ms. ${nombre}`;
+            }else if(genero==="Male"){
+                return `Hello, Mr. ${nombre}`;
+            }
+        }
+        return `Hello, ${nombre}`;
+    }
+}
+
+
+
 export { saludador, saludadorConNombre, saludadorConHora, saludadorSegunElGenero, saludadorSegunEdadYGenero};
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -2,39 +2,29 @@ import { saludador,saludadorConNombre, saludadorConHora, saludadorSegunElGenero,
 
 describe("Saludador:", () => {
     it("deberia saludar con 'Hola'", () => {
-        expect(saludador()).toEqual("Hola");
+        expect(saludador("español")).toEqual("Hola");
+        expect(saludador("ingles")).toEqual("Hello");
     });
 
     it("deberia saludar con 'Hola' incluyendo el nombre", () => {
-        expect(saludadorConNombre("Diana")).toEqual("Hola Diana");
-    });
-
-    it("deberia saludar con 'Hola' incluyendo el nombre", () => {
-        expect(saludadorConNombre("Diana")).toEqual("Hola Diana");
+        expect(saludadorConNombre("Diana","español")).toEqual("Hola Diana");
+        expect(saludadorConNombre("Leonardo","ingles")).toEqual("Hello Leonardo");
     });
 
     it("deberia saludar con segun la hora", () => {
-        expect(saludadorConHora(10)).toEqual("Buenos dias");
-    });
-
-    it("deberia saludar con segun la hora", () => {
-        expect(saludadorConHora(15)).toEqual("Buenas tardes");
-    });
-
-    it("deberia saludar con segun la hora", () => {
-        expect(saludadorConHora(20)).toEqual("Buenas noches");
+        expect(saludadorConHora(10, "español")).toEqual("Buenos dias");
+        expect(saludadorConHora(10, "ingles")).toEqual("Good morning");
     });
 
     it("deberia saludar con segun el genero", () => {
-        expect(saludadorSegunElGenero("Fabiola","Femenino")).toEqual("Hola, Sra. Fabiola");
-    });
-
-    it("deberia saludar con segun el genero", () => {
-        expect(saludadorSegunElGenero("Marcos","Masculino")).toEqual("Hola, Sr. Marcos");
+        expect(saludadorSegunElGenero("Fabiola","Femenino","español")).toEqual("Hola, Sra. Fabiola");
+        expect(saludadorSegunElGenero("Marcos","Male","ingles")).toEqual("Hello, Mr. Marcos");
     });
 
     it("deberia saludar con segun la edad", () => {
-        expect(saludadorSegunEdadYGenero("Felipe",40,"Masculino")).toEqual("Hola, Sr. Felipe");
-        expect(saludadorSegunEdadYGenero("Emilia",28,"Femenino")).toEqual("Hola, Emilia");
+        expect(saludadorSegunEdadYGenero("Felipe",40,"Masculino","español")).toEqual("Hola, Sr. Felipe");
+        expect(saludadorSegunEdadYGenero("Emilia",28,"Femenino","español")).toEqual("Hola, Emilia");
+        expect(saludadorSegunEdadYGenero("Britney",40,"Female","ingles")).toEqual("Hello, Ms. Britney");
+        expect(saludadorSegunEdadYGenero("Jake",28,"Male","ingles")).toEqual("Hello, Jake");
     });
 });
