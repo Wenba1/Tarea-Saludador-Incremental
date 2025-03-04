d3bc339a7e2284abbf1e9cee8ad6c83bcd620475
Wendy Balderrama <balderramawendym@gmail.com>
2025-03-04 18:47:20 -0400

    Style: Arregle el index y presenter para mostrar el saludador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,46 @@
-import sumar from "./sumador";
+import { 
+  saludador, 
+  saludadorConNombre, 
+  saludadorConHora, 
+  saludadorSegunElGenero, 
+  saludadorSegunEdadYGenero 
+} from "./saludador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const nombreInput = document.querySelector("#nombre");
+const idiomaSelect = document.querySelector("#idioma");
+const horaInput = document.querySelector("#hora");
+const generoSelect = document.querySelector("#genero");
+const edadInput = document.querySelector("#edad");
+const form = document.querySelector("#saludar-form");
+const divSaludo = document.querySelector("#saludo");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const nombre = nombreInput.value;
+  const idioma = idiomaSelect.value;
+  const hora = horaInput.value ? Number(horaInput.value) : new Date().getHours();
+  const genero = generoSelect.value;
+  const edad = Number(edadInput.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  let saludo = "";
+
+  // Combinando las funciones según los campos llenados
+  if (nombre) {
+    saludo = saludadorConNombre(nombre, idioma);
+  }
+
+  if (hora !== undefined) {
+    saludo = saludadorConHora(hora, idioma);
+  }
+
+  if (genero) {
+    saludo = saludadorSegunElGenero(nombre, genero, idioma);
+  }
+
+  if (edad) {
+    saludo = saludadorSegunEdadYGenero(nombre, edad, genero, idioma);
+  }
+
+  divSaludo.innerHTML = `<p>${saludo}</p>`;
 });
